f234c9684105a8612ccab61b5968a514ad9558de
Nick Sette <njs2394@gmail.com>
2023-11-26 17:54:57 -0500

    dawgs

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import react, { useEffect, useState } from "react";
+
+const dogUrl = "https://dog.ceo/api/breeds/image/random"
+
+function App() {
+
+    const [image, setImage] = useState("Loading...")
+
+    useEffect(() => {
+        fetch(dogUrl)
+            .then(res => res.json())
+            .then(dogImage => {
+                console.log(dogImage.message)
+                setImage(<img src={dogImage.message} alt={"A Random Dog"}></img>)
+
+            })
+
+    }, [])
+
+
+    return (
+
+        <div>
+            <p>{image}</p>
+        </div>
+    )
+
+}
+
+
+
+export default App;
